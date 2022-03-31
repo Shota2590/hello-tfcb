ba0ced6ab428d7024888a8ebe35d5c90806c918e
Shota2590 <shota.saito.s7@jalinfotec.co.jp>
2022-03-31 17:01:38 +0900

    modularization

--- main.tf
+++ main.tf
@@ -1,82 +1,20 @@
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "saito_org"
-    workspaces {
-      name = "hello-tf"
-    }
-  }
+
 }
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
+	access_key = var.access_key
+	secret_key = var.secret_key
+	region     = var.region
 }
 
-resource "aws_vpc" "hashicat" {
-  cidr_block           = var.address_space
-  enable_dns_hostnames = true
+//--------------------------------------------------------------------
+// Modules
+module "devinstance" {
+	source  = "app.terraform.io/s_saito_org/devinstance/aws"
+	version = "0.0.3"
+
+	hello_tf_instance_type = "t2.small"
+      prefix = "ShotaSaito"
 }
 
-resource "aws_subnet" "hashicat" {
-  vpc_id     = aws_vpc.hashicat.id
-  cidr_block = var.subnet_prefix
-}
-
-resource "aws_instance" "hashicat" {
-  ami                         = var.ami
-  instance_type               = var.hello_tf_instance_type
-  subnet_id                   = aws_subnet.hashicat.id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.hashicat.id]
-}
-
-resource "aws_security_group" "hashicat" {
-  name = "hashicat-security-group"
-
-  vpc_id = aws_vpc.hashicat.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
-}
-
-resource "aws_internet_gateway" "hashicat" {
-  vpc_id = aws_vpc.hashicat.id
-}
-
-resource "aws_route_table" "hashicat" {
-  vpc_id = aws_vpc.hashicat.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.hashicat.id
-  }
-}
-
-resource "aws_route_table_association" "hashicat" {
-  subnet_id      = aws_subnet.hashicat.id
-  route_table_id = aws_route_table.hashicat.id
-}
-
-resource "aws_eip" "hashicat" {
-  instance = aws_instance.hashicat.id
-  vpc      = true
-}
-
-resource "aws_eip_association" "hashicat" {
-  instance_id   = aws_instance.hashicat.id
-  allocation_id = aws_eip.hashicat.id
-}
